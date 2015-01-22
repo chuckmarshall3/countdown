99dbc708eebce9382d95080b30c20bf7815defe8
chuckmarshall3 <chuckmarshall3@gmail.com>
2015-01-21 22:43:04 -0700

    completed countdown php and js

--- js/countdown.js
+++ js/countdown.js
@@ -1,11 +1,40 @@
-//change the text below to reflect your own,
-var before = "Groundhog's Day!";
-var current = "Today is Groundhog's Day!";
-var montharray = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+// Function to complete.
+function CountDownTimer(dateTime, divId) {
+    var end     = new Date(dateTime);
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour   = _minute * 60;
+    var _day    = _hour * 24;
+    var timer;
 
-function countdown(yr, m, d) {
- // ...
+    function showRemaining() {
+
+		var currentTime = new Date();
+
+       	var timeDiff=(end.getTime() - currentTime.getTime());
+
+       	var days = Math.floor(timeDiff/_day);
+       	timeDiff-=days*_day;
+
+       	var hours = Math.floor(timeDiff/_hour);
+       	timeDiff-=hours*_hour;
+
+       	var minutes = Math.floor(timeDiff/_minute);
+       	timeDiff-=minutes*_minute;
+
+       	var seconds = Math.floor(timeDiff/_second);
+       	timeDiff-=seconds*_second;
+
+       	
+       	document.getElementById(divId).value = "There are "+days+" Days, "+hours+" Hour(s), "+minutes+" Minute(s), "+seconds+" Second(s) Until "+dateTime;
+
+      
+    }
+    timer = setInterval(function(){ showRemaining() }, 1000);
+
+    
 }
 
-//enter the count down date using the format year/month/day
-countdown(2015, 2, 2);
+// Enter the date and time as a string in the following format.  Second parameter is the HTML id of element where you want
+//    the countdown to appear.
+CountDownTimer('02/02/2015 12:00 AM', 'countdown');
--- php/countdown.php
+++ php/countdown.php
@@ -4,11 +4,35 @@ if (PHP_SAPI !== 'cli') {
     die (PHP_EOL.'Please run '.__FILE__.' from the command-line.'.PHP_EOL.PHP_EOL);
 }
 
+
+//I know that I could have done this simpler with built in sleep() and loop
+//but wanted to do it a little different and try using clean code method
+//and output server time
+
+function checktime($seconds, $starttime, $endtime){
+
+	if($endtime-$seconds==$starttime){
+    	echo "\n".$seconds." server time=".date('H:i:s');
+    	$seconds--;
+    }
+    return $seconds;
+}
+
 function countdown($seconds)
 {
-    // ...
+
+	$starttime=time();
+    $endtime=time() + 5;
+
+    while($starttime < $endtime && $seconds >= 0 ){
+	
+    	$seconds = checktime($seconds, $starttime, $endtime);
+		$starttime=time();
+    }
 }
 
 countdown(5);
+echo "\n";	
+die('0 server time='.date('H:i:s').' All done...');
 
-die('0 All done...');
+?>
